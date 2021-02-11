f0919ac3f67c54a9a26a1793b0708ad6cde703ea
triniwiz <fortune.osei@yahoo.com>
2021-02-11 14:29:47 -0400

    fix: update guards

--- packages/nativescript-nested-scrollview/index.android.ts
+++ packages/nativescript-nested-scrollview/index.android.ts
@@ -4,174 +4,167 @@ import { ScrollEventData, ContentView } from '@nativescript/core';
 import { Orientation } from '@nativescript/core/ui/layouts/stack-layout';
 import { layout } from '@nativescript/core/utils';
 
-const scrollBarIndicatorVisibleProperty = new Property<NestedScrollView,
-    boolean>({
-        name: 'scrollBarIndicatorVisible',
-        defaultValue: true,
-        valueConverter: booleanConverter
-    });
+const scrollBarIndicatorVisibleProperty = new Property<NestedScrollView, boolean>({
+	name: 'scrollBarIndicatorVisible',
+	defaultValue: true,
+	valueConverter: booleanConverter,
+});
 
-const converter = makeParser<Orientation>(
-    makeValidator('horizontal', 'vertical')
-);
+const converter = makeParser<Orientation>(makeValidator('horizontal', 'vertical'));
 export const orientationProperty = new Property<NestedScrollView, Orientation>({
-    name: 'orientation',
-    defaultValue: 'vertical',
-    affectsLayout: true,
-    valueChanged: (
-        target: NestedScrollView,
-        oldValue: Orientation,
-        newValue: Orientation
-    ) => {
-        target._onOrientationChanged();
-    },
-    valueConverter: converter
+	name: 'orientation',
+	defaultValue: 'vertical',
+	affectsLayout: true,
+	valueChanged: (target: NestedScrollView, oldValue: Orientation, newValue: Orientation) => {
+		target._onOrientationChanged();
+	},
+	valueConverter: converter,
 });
 
 export class NestedScrollView extends ContentView {
-    public static scrollEvent = 'scroll';
-    public orientation: Orientation;
-    public scrollBarIndicatorVisible: boolean;
-    private _androidViewId: number = -1;
-    private handler;
+	public static scrollEvent = 'scroll';
+	public orientation: Orientation;
+	public scrollBarIndicatorVisible: boolean;
+	private _androidViewId: number = -1;
+	private handler;
 
-    public createNativeView() {
-        return new androidx.core.widget.NestedScrollView(
-            this._context
-        );
-    }
+	public createNativeView() {
+		return new androidx.core.widget.NestedScrollView(this._context);
+	}
 
-    public initNativeView(): void {
-        if (this._androidViewId < 0) {
-            this._androidViewId = android.view.View.generateViewId();
-        }
-        this.nativeViewProtected.setId(this._androidViewId);
-    }
+	public initNativeView(): void {
+		if (this._androidViewId < 0) {
+			this._androidViewId = android.view.View.generateViewId();
+		}
+		this.nativeViewProtected.setId(this._androidViewId);
+		const that = new WeakRef(this);
+		this.handler = new androidx.core.widget.NestedScrollView.OnScrollChangeListener({
+			onScrollChange(view, scrollX: number, scrollY: number, oldScrollX: number, oldScrollY: number) {
+				const owner = that.get();
+				if (owner) {
+					owner.notify(<ScrollEventData>{
+						object: this,
+						eventName: NestedScrollView.scrollEvent,
+						scrollX: scrollX,
+						scrollY: scrollY,
+					});
+				}
+			},
+		});
+	}
 
-    @profile
-    public onLoaded() {
-        super.onLoaded();
-        const that = new WeakRef(this);
-        this.handler = new androidx.core.widget.NestedScrollView.OnScrollChangeListener(
-            {
-                onScrollChange(
-                    view,
-                    scrollX: number,
-                    scrollY: number,
-                    oldScrollX: number,
-                    oldScrollY: number
-                ) {
-                    const owner = that.get();
-                    if (owner) {
-                        owner.notify(<ScrollEventData>{
-                            object: this,
-                            eventName: NestedScrollView.scrollEvent,
-                            scrollX: scrollX,
-                            scrollY: scrollY
-                        });
-                    }
-                }
-            }
-        );
-        this.nativeView.setOnScrollChangeListener(this.handler);
-    }
+	@profile
+	public onLoaded() {
+		super.onLoaded();
+		if (this.nativeViewProtected) {
+			this.nativeViewProtected.setOnScrollChangeListener(this.handler);
+		}
+	}
 
-    public onUnloaded() {
-        this.nativeView.setOnScrollChangeListener(null);
-        this.handler = null;
-        super.onUnloaded();
-    }
+	public onUnloaded() {
+		if (this.nativeViewProtected) {
+			this.nativeViewProtected.setOnScrollChangeListener(null);
+		}
+		super.onUnloaded();
+	}
 
-    public _onOrientationChanged() {
-        if (this.nativeViewProtected) {
-            const parent = this.parent;
-            if (parent) {
-                parent._removeView(this);
-                parent._addView(this);
-            }
-        }
-    }
+	disposeNativeView() {
+		this.handler = null;
+		super.disposeNativeView();
+	}
 
-    [scrollBarIndicatorVisibleProperty.getDefault](): boolean {
-        return true;
-    }
+	public _onOrientationChanged() {
+		if (this.nativeViewProtected) {
+			const parent = this.parent;
+			if (parent) {
+				parent._removeView(this);
+				parent._addView(this);
+			}
+		}
+	}
 
-    [scrollBarIndicatorVisibleProperty.setNative](value: boolean) {
-        if (this.orientation === 'horizontal') {
-            this.nativeViewProtected.setHorizontalScrollBarEnabled(value);
-        } else {
-            this.nativeViewProtected.setVerticalScrollBarEnabled(value);
-        }
-    }
+	[scrollBarIndicatorVisibleProperty.getDefault](): boolean {
+		return true;
+	}
 
-    public scrollToVerticalOffset(value: number, animated: boolean) {
-        const nativeView = this.nativeViewProtected;
-        if (nativeView && this.orientation === 'vertical') {
-            value *= layout.getDisplayDensity();
+	[scrollBarIndicatorVisibleProperty.setNative](value: boolean) {
+		if (this.nativeViewProtected) {
+			if (this.orientation === 'horizontal') {
+				this.nativeViewProtected.setHorizontalScrollBarEnabled(value);
+			} else {
+				this.nativeViewProtected.setVerticalScrollBarEnabled(value);
+			}
+		}
+	}
 
-            if (animated) {
-                nativeView.smoothScrollTo(0, value);
-            } else {
-                nativeView.scrollTo(0, value);
-            }
-        }
-    }
+	public scrollToVerticalOffset(value: number, animated: boolean) {
+		const nativeView = this.nativeViewProtected;
+		if (nativeView && this.orientation === 'vertical') {
+			value *= layout.getDisplayDensity();
 
-    public scrollToHorizontalOffset(value: number, animated: boolean) {
-        const nativeView = this.nativeViewProtected;
-        if (nativeView && this.orientation === 'horizontal') {
-            value *= layout.getDisplayDensity();
+			if (animated) {
+				nativeView.smoothScrollTo(0, value);
+			} else {
+				nativeView.scrollTo(0, value);
+			}
+		}
+	}
 
-            if (animated) {
-                nativeView.smoothScrollTo(value, 0);
-            } else {
-                nativeView.scrollTo(value, 0);
-            }
-        }
-    }
+	public scrollToHorizontalOffset(value: number, animated: boolean) {
+		const nativeView = this.nativeViewProtected;
+		if (nativeView && this.orientation === 'horizontal') {
+			value *= layout.getDisplayDensity();
 
-    get horizontalOffset(): number {
-        const nativeView = this.nativeViewProtected;
-        if (!nativeView) {
-            return 0;
-        }
+			if (animated) {
+				nativeView.smoothScrollTo(value, 0);
+			} else {
+				nativeView.scrollTo(value, 0);
+			}
+		}
+	}
 
-        return nativeView.getScrollX() / layout.getDisplayDensity();
-    }
+	get horizontalOffset(): number {
+		const nativeView = this.nativeViewProtected;
+		if (!nativeView) {
+			return 0;
+		}
 
-    get verticalOffset(): number {
-        const nativeView = this.nativeViewProtected;
-        if (!nativeView) {
-            return 0;
-        }
+		return nativeView.getScrollX() / layout.getDisplayDensity();
+	}
 
-        return nativeView.getScrollY() / layout.getDisplayDensity();
-    }
+	get verticalOffset(): number {
+		const nativeView = this.nativeViewProtected;
+		if (!nativeView) {
+			return 0;
+		}
 
-    get scrollableWidth(): number {
-        const nativeView = this.nativeViewProtected;
-        if (!nativeView || this.orientation !== 'horizontal') {
-            return 0;
-        }
+		return nativeView.getScrollY() / layout.getDisplayDensity();
+	}
 
-        return this.getScrollableLength() / layout.getDisplayDensity();
-    }
+	get scrollableWidth(): number {
+		const nativeView = this.nativeViewProtected;
+		if (!nativeView || this.orientation !== 'horizontal') {
+			return 0;
+		}
 
-    get scrollableHeight(): number {
-        const nativeView = this.nativeViewProtected;
-        if (!nativeView || this.orientation !== 'vertical') {
-            return 0;
-        }
+		return this.getScrollableLength() / layout.getDisplayDensity();
+	}
 
-        return this.getScrollableLength() / layout.getDisplayDensity();
-    }
+	get scrollableHeight(): number {
+		const nativeView = this.nativeViewProtected;
+		if (!nativeView || this.orientation !== 'vertical') {
+			return 0;
+		}
 
-    private getScrollableLength(): number {
-        if (!this.nativeView) {
-            return 0;
-        }
-        return this.nativeView.getChildAt(0).getHeight();
-    }
+		return this.getScrollableLength() / layout.getDisplayDensity();
+	}
+
+	private getScrollableLength(): number {
+		if (!this.nativeView) {
+			return 0;
+		}
+		return this.nativeView.getChildAt(0).getHeight();
+	}
 }
 
 scrollBarIndicatorVisibleProperty.register(NestedScrollView);
