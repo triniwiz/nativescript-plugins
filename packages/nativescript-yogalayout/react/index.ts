1cc4810db69fa802e0f8b71d0772b84becfde2cb
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-05-22 15:34:49 +0100

    fix(react): Improve typings

--- packages/nativescript-yogalayout/react/index.ts
+++ packages/nativescript-yogalayout/react/index.ts
@@ -13,7 +13,7 @@ export function registerYogaLayout(): void {
 
 export { View, RNViewProps } from "./View";
 
-export type YogaAttributes = ViewAttributes & {
+interface YogaDistinctAttributes {
   /**
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
@@ -34,7 +34,15 @@ export type YogaAttributes = ViewAttributes & {
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
   padding?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
-
+  /**
+   * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
+   */
+  paddingVertical?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
+  /**
+   * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
+   */
+  paddingHorizontal?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
+  
   /**
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
@@ -55,7 +63,15 @@ export type YogaAttributes = ViewAttributes & {
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
   margin?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
-
+  /**
+   * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
+   */
+  marginHorizontal?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
+  /**
+   * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
+   */
+  marginVertical?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
+  
   /**
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
@@ -80,7 +96,7 @@ export type YogaAttributes = ViewAttributes & {
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
   end?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
-
+  
   /**
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
@@ -89,30 +105,64 @@ export type YogaAttributes = ViewAttributes & {
    * Number values are interpreted as display-independent pixels. Will no-op if set to "auto".
    */
   maxWidth?: string | number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
-
+  
   alignItems?: "stretch" | "center" | "flex-end" | "flex-start" | "baseline";
   overflow?: "visible" | "hidden" | "scroll";
   position?: "relative" | "absolute";
   alignSelf?: "stretch" | "center" | "flex-end" | "flex-start" | "baseline" | "auto";
   flexGrow?: number;
   flexShrink?: number;
-
+  
   /**
    * Number values are interpreted as display-independent pixels.
    */
   flexBasis?: number | LengthDipUnit | LengthPxUnit | LengthPercentUnit;
   flex?: number;
   flexDirection?: "row" | "column" | "column-reverse" | "row-reverse";
+  /**
+   * TODO: Change the implementation to accept a cross-platform "rtl" | "ltr" | "inherit" enum.
+   * @platform iOS     YGDirection.RTL | YGDirection.LTR | YGDirection.Inherit
+   * @platform android com.facebook.yoga.YogaDirection.RTL | com.facebook.yoga.YogaDirection.LTR | com.facebook.yoga.YogaDirection.INHERIT
+   */
+  direction?: any;
   alignContent?: "stretch" | "center" | "flex-end" | "flex-start" | "baseline";
-
+  
   flexWrap?: "no-wrap" | "wrap" | "wrap-reverse";
   justifyContent?: "flex-start" | "flex-end" | "center" | "space-around" | "space-between" | "space-evenly";
-};
+}
+
+export type YogaAttributes = ViewAttributes & YogaDistinctAttributes;
 
 declare global {
   module JSX {
     interface IntrinsicElements {
       yoga: NativeScriptProps<YogaAttributes, YogaLayout>;
     }
+  }
+}
+
+declare module "react-nativescript" {
+  /**
+   * @see the augmentation of import("@nativescript/core").Style made by the plugin in ../common.ts.
+   */
+  interface RNSStyle {
+    left?: YogaDistinctAttributes["left"];
+    top?: YogaDistinctAttributes["top"];
+    right?: YogaDistinctAttributes["right"];
+    bottom?: YogaDistinctAttributes["bottom"];
+    start?: YogaDistinctAttributes["start"];
+    end?: YogaDistinctAttributes["end"];
+    marginVertical?: YogaDistinctAttributes["marginVertical"];
+    marginHorizontal?: YogaDistinctAttributes["marginHorizontal"];
+    paddingHorizontal?: YogaDistinctAttributes["paddingHorizontal"];
+    paddingVertical?: YogaDistinctAttributes["paddingVertical"];
+    justifyContent?: YogaDistinctAttributes["justifyContent"];
+    maxWidth?: YogaDistinctAttributes["maxWidth"];
+    maxHeight?: YogaDistinctAttributes["maxHeight"];
+    flex?: YogaDistinctAttributes["flex"];
+    overflow?: YogaDistinctAttributes["overflow"];
+    position?: YogaDistinctAttributes["position"];
+    flexBasis?: YogaDistinctAttributes["flexBasis"];
+    direction?: YogaDistinctAttributes["direction"];
   }
 }
