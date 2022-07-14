66ed70bd1c68e3db5f4d95699c3851ffc0f47fbe
Eduardo Speroni <edusperoni@users.noreply.github.com>
2022-07-13 22:49:09 -0300

    fix: do not use require in angular packages (#125)

--- packages/nativescript-youtubeplayer/angular/nativescript-youtubeplayer.directives.ts
+++ packages/nativescript-youtubeplayer/angular/nativescript-youtubeplayer.directives.ts
@@ -3,7 +3,6 @@ import { Directive } from '@angular/core';
 @Directive({
 	selector: 'YoutubePlayer',
 })
-// @ts-ignore
 export class YoutubePlayerDirective {}
 
 export const DIRECTIVES = [YoutubePlayerDirective];
