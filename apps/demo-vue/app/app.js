1ec1968726bc84366e7a635b3f588cac3c0373fd
Nathan Walker <walkerrunpdx@gmail.com>
2024-10-22 16:46:43 -0700

    chore: nx migrate @nativescript/plugin-tools (#205)

--- apps/demo-vue/app/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from "nativescript-vue";
-import View from '@triniwiz/nativescript-yogalayout/vue';
-
-Vue.use(View);
-import Home from "./components/Home";
-
-new Vue({
-    render: h => h('frame', [h(Home)]),
-}).$start();
